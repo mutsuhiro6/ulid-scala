6823e3a76595ae30287e8487e9b21cfcc7cb7f96
Mutsuhiro Iwamoto <mutsuhiro6@icloud.com>
2022-02-04 00:03:39 +0900

    add jmh benchmark

---
+++ benchmark/src/main/scala/ULIDGen.scala
@@ -0,0 +1,52 @@
+package benchmark.ulid
+
+import com.chatwork.scala.ulid.{ULID => ChatworkULID}
+import de.huxhorn.sulky.ulid.{ULID => SulkyULID}
+import net.petitviolet.ulid4s.{ULID => PetitvioletULID}
+import com.github.mutsuhiro6.util.ulid.{ULID => M6ULID}
+import com.github.mutsuhiro6.util.ulid.{MonotonicULID => M6MonoULID}
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.State
+import wvlet.airframe.ulid.{ULID => AirframeULID}
+
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.SampleTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+class ULIDGen :
+
+  @Benchmark
+  def javaUUID =
+    UUID.randomUUID.toString
+
+  @Benchmark
+  def airframeULID =
+    AirframeULID.newULIDString
+  
+  @Benchmark
+  def petitvioletULID =
+    PetitvioletULID.generate
+  
+  @Benchmark
+  def chatworkULID =
+    ChatworkULID.generate().asString
+  
+  val sulkyULIDGen = new SulkyULID
+  @Benchmark
+  def sulkyULID =
+    sulkyULIDGen.nextULID
+
+  @Benchmark
+  def m6ULID =
+    M6ULID.randomULID.toString
+  
+  // @Benchmark
+  // def mutsuhiroMonoULID =
+  //   M6MonoULID.randomULID.toString
+
--- build.sbt
+++ build.sbt
@@ -1,12 +1,35 @@
 val scala3Version = "3.1.0"
 
-lazy val root = project
-  .in(file("."))
+lazy val lib = project
+  .in(file("lib"))
   .settings(
     name := "ulid-scala3",
     version := "0.1.0-SNAPSHOT",
     organization := "com.github.mutsuhiro6",
     scalaVersion := scala3Version,
-    
     libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % "test"
   )
+
+// For benchmark module
+val scala2Version = "2.13.6"
+
+lazy val benchmark = project
+  .in(file("benchmark"))
+  .settings(
+    name := "ulid-benchmark",
+    publish / skip := true,
+    libraryDependencies ++= Seq(
+      "com.chatwork" % "scala-ulid_2.13" % "1.0.24",
+      "org.wvlet.airframe" %% "airframe-ulid" % "21.12.1",
+      "net.petitviolet" % "ulid4s_2.13" % "0.5.0",
+      "de.huxhorn.sulky" % "de.huxhorn.sulky.ulid" % "8.3.0"
+    ),
+    scalaVersion := scala3Version,
+    crossScalaVersions := Seq(scala3Version, scala2Version)
+  )
+  .enablePlugins(JmhPlugin)
+  .dependsOn(lib)
+
+lazy val root = project
+  .in(file("."))
+  .aggregate(lib)
